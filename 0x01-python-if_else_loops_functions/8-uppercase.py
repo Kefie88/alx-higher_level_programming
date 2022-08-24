7b089ddbfbdaa390965c20f3b4ac81f14ded5fdc
Kefyalew <you@example.com>
2022-08-24 05:49:35 +0000

    If Else Loop Commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         uni_code = ord(str[i])
-        if uni_code >= 97 and uni_code <=122:
+        if uni_code >= 97 and uni_code <= 122:
             uni_code = uni_code - 32
         print("{}".format(chr(uni_code)), end='')
     print()
