6f90902c5b020cc799f1e4f35efe4512bc305293
Kefyalew <you@example.com>
2022-08-27 10:34:03 +0000

    Python Data Structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
- * main - check the code for ALX School Students.
- * Return: Always 0
+ * is_palindrome - checks if a single linked list is a palindrome
+ * @head: doublevpointer to list
+ * Return: 1 if palindrome otherwise 0
  */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
-	return (0);
+	return (checkPalindrome(head, *head));
+}
+/**
+ * checkpalindrome - recursive function to check if list is a palindrome
+ * @headptr: double pointer to list
+ * @tptr: pointer to list
+ * Return: 1 or 0
+ */
+int checkPalindrome(listint_t **headptr, listint_t *tptr)
+{
+	int ret;
+
+	if (tptr == NULL)
+		return (1);
+	ret = checkPalindrome(headptr, tptr->next) && ((*headptr)->n == tptr->n);
+	return (ret);
 }
--- 0x03-python-data_structures/ll.h
+++ 0x03-python-data_structures/ll.h
@@ -1,6 +1,5 @@
 #ifndef lists_h
 #define lists_h
-#include <stdlib.h>
 /**
  * struct listint_s - single linked list
  * @n: integer
@@ -17,5 +16,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-int checkPalindrome(listint_t **head, listint_t *tptr);
 #endif
