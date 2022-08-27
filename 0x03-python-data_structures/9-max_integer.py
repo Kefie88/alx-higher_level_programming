c60fcb0b631d7e58a56fa5f38a9f09824cd1151e
Kefyalew <you@example.com>
2022-08-27 07:55:15 +0000

    Python Data Structure Commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin.python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
+        return None
+    lar = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > lar:
+            lar = my_list[i]
+    return (lar)
