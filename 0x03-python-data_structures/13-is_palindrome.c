b5103105aedacaeda2243a70814e3129c131cdc2
Kefyalew <you@example.com>
2022-08-27 07:51:31 +0000

    Python Data Structure Commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * main - check the code for ALX School Students.
+ * Return: Always 0
+ */
+int main(void)
+{
+	listint_t *head;
+	return (0);
+}
