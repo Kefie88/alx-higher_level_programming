b75f89154668d7f96a5e12da3bbae75aa3eea753
Kefyalew <you@example.com>
2022-08-27 08:07:30 +0000

    Python Data Structure Commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def multiple_return(sentence):
+def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else "None"
     tup = length, first_char
