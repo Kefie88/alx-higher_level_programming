f87fee8dd1074d734e38c1a490ab5d9cea694de8
Kefyalew <you@example.com>
2022-08-27 08:32:13 +0000

    Python Data Structure Commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,19 @@
 #include <Python.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
  * print_python_list_info - Prints information about python objects
- * @p: PyObject pointer to print info about Compile With:
- * gcc -Wall -Werror -Wextra - pedantic -std=c99 -shared
- * (* -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
- *  (* 100-print_python_list_info.c
+ * @p: PyObject pointer to print
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i, py_list_size;
-	PyObject *item;
-	const char *item_type;
-	PyListObject *list_object_cast;
+	int i;
 
-	list_object_cast = (PyListObject *)p;
-	py_list_size = PyList_Size(p);
-	printf("[*] Size of the Python List = %d\n", (int) py_list_size);
-	printf("[*] Allocated = %d\n", (int)list_object_cast->allocated);
-	for (i = 0; i < py_list_size; i++)
+	printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
 	{
-		item = PyList_GetItem(p, i);
-		item_type = Py_TYPE(item)->to_name;
-		printf("Element %d: %s\n", (int) i, item_type);
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 	}
 }
