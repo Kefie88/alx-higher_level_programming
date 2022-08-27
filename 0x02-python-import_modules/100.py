e5425134f2caa03712643e4a4afcc298c33accf0
Kefyalew <you@example.com>
2022-08-27 10:08:25 +0000

    Python Import Modules

--- 0x02-python-import_modules/100.py
+++ 0x02-python-import_modules/100.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv
-
-if __name__ == "__main__":
-    length = len(argv)
-    if length != 4:
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if length == 4:
-        op = argv[2]
-        a = int(argv[1])
-        b = int(argv[3])
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
     if op == '+':
         print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
     elif op == '-':
@@ -22,3 +19,6 @@ if __name__ == "__main__":
     else:
          print("Unknown operator. Available operators: +, -, * and /")
          exit(1)
+if __name__ == "__main__":
+    import sys
+    arg_calc(sys.argv)
