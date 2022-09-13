1793cf88d2bf06b0fd6224eddb1eb84f060cf5e0
Kefyalew <you@example.com>
2022-09-13 10:33:03 +0000

    Python Classes Commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
+    """Represent a square."""
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
     @size.setter
     def size(self, value):
@@ -13,8 +21,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
+        """Return the currrent area of the square."""
         return (self.__size * self.__size)
     def my_print(self):
+        """print the square with the # character."""
         for i in range(0, self.__size):
             [print('#', end="") for j in range(self.__size)]
             print("")
