c012115802e6c2b618493851fd33b2692cd68f4b
Kefyalew <you@example.com>
2022-09-13 12:27:15 +0000

    Python Classes Commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
+
 class Square:
     """Represent a squarre."""
+
     def __init__(self, size):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
