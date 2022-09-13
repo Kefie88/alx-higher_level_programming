c1fee64bbbf3786035b69d148e8b72027be77885
Kefyalew <you@example.com>
2022-09-13 10:19:48 +0000

    Python Classes Commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Define a class square."""
 class Square:
+    """Represent a square."""
     def __init(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
     @property
     def size(self):
