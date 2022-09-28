27e52941be62786dfba229fece9872dc373aa5e1
Kefyalew <you@example.com>
2022-09-28 12:09:42 +0000

    Python Input Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Args:
         filename(any): name of the file
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
