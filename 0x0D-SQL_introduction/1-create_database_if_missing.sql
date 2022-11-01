d9cb208da1482e797171c0b27b38bf892ee411f4
Kefyalew <you@example.com>
2022-11-01 09:45:36 +0000

    SQL Introduction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,2 @@
 --Script creates the database hbtn_0c_0
---If the database hbtn_0c_0 exists, script should not fail
---you are not allowed to use SELECT and SHOW statements
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
