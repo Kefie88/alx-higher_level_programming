e8b2d0892d8d7d77838b5dec3b563beab37d2b51
Kefyalew <you@example.com>
2022-11-01 14:21:44 +0000

    SQL Introduction

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,7 @@
 -- Displays average temperature
 -- By city order by descending temperature
-SELECT 'city', AVG('value') AS 'avg_temp'
-FROM 'temperatures'
-GROUP BY 'city'
-ORDER BY 'avg_temp' DESC;
+SELECT city,
+AVG(value) AS 'avg_temp'
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
