661b022f43efd127fc2fb5610700e0db7b011abe
Kefyalew <you@example.com>
2022-11-02 11:58:44 +0000

    SQL More Queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,11 @@
 -- Creates database hbtn_0d_2 and user user_0d_2
 -- user_0d_2 should have only SELECT privilege
 -- user_0d_2 password should be set to user_02_2_pwd
-CREATE DATABASE IF NOT EXISTS 'hbtn_02_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE DATABASE
+	IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+	IF NOT EXISTS 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_od_2'.*
+GRANT SELECT
+	ON 'hbtn_od_2'.*
 	TO 'user_0d_2'@'localhost';
