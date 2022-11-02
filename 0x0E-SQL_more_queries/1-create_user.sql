f0f907082cc5d83119d4f664c2147177cf570a4e
Kefyalew <you@example.com>
2022-11-02 11:50:50 +0000

    SQL More Queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- user_0d_1 should have privileges on your MySQL server
 -- user_0d_1 password should be set to user_0d_1_pwd
 -- If user exists, your script should not fail
-CREATE USER IF NOT EXITS 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
