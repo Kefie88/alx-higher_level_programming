8f74591230afebfdfdee6d8763114c7e8ff8c1fc
Kefyalew <you@example.com>
2022-11-02 12:55:25 +0000

    SQL More Queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
 -- Create database hbtn_0d_usa and the table states
 -- id INT unique, auto generate, can't be null and is primary key
 -- name VARCHAR(256) can't be null
-CREATE DATABASE
-	IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
-	PRIMARY KEY ('id'),
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	PRIMARY KEY(id),
+	id INT 	NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
 );
