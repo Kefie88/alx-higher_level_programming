668fea5f4c4a229e30c1bae385cc89d76ccbca8a
Kefyalew <you@example.com>
2022-11-03 07:34:01 +0000

    Late Commit

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Creates the table force_name on your MySQL server
 -- id INT and name VARCHAR(256) can't be null
-CREATE TABLE IF NOT EXISTS 'force_name'(
-	'id' INT, 'name' VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT, name VARCHAR(256) NOT NULL
 );
