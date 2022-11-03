b6e34060e0f9626c9fcb01495a1c19798a0d9eb9
Kefyalew <you@example.com>
2022-11-03 08:11:52 +0000

    Late Commit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
 -- Convert the entire database hbtn_0c_0 to UTF8
 ALTER DATABASE
 	hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
 
 -- convert table "first_table' to UTF8
 ALTER TABLE
