8ba99af1c377554da81b0b29074f450478597bdb
Kefyalew <you@example.com>
2022-11-03 08:06:25 +0000

    Late Commit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -11,9 +11,8 @@ ALTER TABLE
 	COLLATE utf8mb4_unicode_ci;
 
 -- convert column 'name' to UTF8
-ALTER TABLE
-	hbtn_0c_0.first_table
+ALTER TABLE hbtn_0c_0.first_table
 	MODIFY name
 	VARCHAR(256)
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
