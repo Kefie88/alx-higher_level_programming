0bf5cc808ecd800c5d9ffdeaf87a483f63eab86b
Kefyalew <you@example.com>
2022-11-03 07:36:04 +0000

    Late Commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Creates table id_not_null on your MySQL server
 -- id INT with default value 1 and name VARCHAR(256)
-CREATE TABLE IF NOT EXIST 'id_not_null' (
-	'id' INT DEFAULT 1, 'name' VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1, name VARCHAR(256)
 );
