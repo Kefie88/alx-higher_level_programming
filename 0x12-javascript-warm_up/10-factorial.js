4a36fd2579b2495eb7f67765157153adef3cb4d2
Kefyalew <you@example.com>
2022-11-28 09:27:29 +0000

    JavaScript Warm up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function fact(n) {
+	if (n <= 0) {
+		return 0;
+	} else if (n === 1) {
+		return 1;
+	} else {
+		return (n * fact(n - 1));
+	}
+}
+
+const first = parseInt(process.argv[2]);
+if (isNaN(first)) {
+	console.log('1');
+} else {
+	console.log(fact(first));
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let printXtimes = parseInt(process.argv[2]);
+if (isNaN(printXtimes) || process.argv[2] === undefined) {
+	console.log('Missing number of occurrences');
+} else {
+	while (printXtimes > 0) {
+		console.log('C is fun');
+		printXtimes--;
+	}
+}
