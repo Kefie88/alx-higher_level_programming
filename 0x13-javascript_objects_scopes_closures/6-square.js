f24436b51a40fcb6e75a64b6c04559664994ed40
Kefyalew <you@example.com>
2022-11-29 13:08:39 +0000

    Javascript Objects Scope Closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,17 @@
 const square_ = require('./5-square');
 const square = class square extends square_ {
   charPrint (c) {
-    if (C) {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0, j < this.height; j++){
+    if (c) {
+      let prints = '';
+      for (let t = 0; t < this.height; t++) {
+        for (let t = 0; t < this.height; t++){
           prints += c;
 	} 
         console.log(prints);
         prints = '';
       } 
     } else {
-      supe.print();
+      super.print();
       }
   }
 };
