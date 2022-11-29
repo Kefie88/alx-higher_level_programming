e53f2e8fcad16f789b6443754011d99ae7549265
Kefyalew <you@example.com>
2022-11-29 13:12:45 +0000

    Javascript Objects Scope Closures

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  return list.filter((value) => (value === searchElement)).lenght;
+  return list.filter((value) => (value === searchElement)).length;
 };
