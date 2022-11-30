fa2d101fef0af6e687e6d740de5cd6eb27a2f423
Kefyalew <you@example.com>
2022-11-30 09:33:37 +0000

    Late Commit

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const Rectangle = require('./4-rectangle');
-const square = class square extends Rectangle {
+module.exports = class square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 };
-module.exports = square;
