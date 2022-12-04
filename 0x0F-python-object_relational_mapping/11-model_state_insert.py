4ef006cca0f14bb96b2534f2a3ce9568ba972bf4
Kefyalew <you@example.com>
2022-12-04 10:26:50 +0000

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 
-"""Prints the State object with the name passed
-as argument from the DB
-"""
+"""Inserts a new State object "Louisiana" to the DB"""
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-if __name__ =="__main__":
+if __name__ == "_main__":
     engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost/{}"
             .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id)
-    found = 0
-    for state in result:
-        if state.name == argv[4]:
-            print("{}".format(state.id))
-            found = 1
-            break
-    if found == 0:
-        print("Not found")
+
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
