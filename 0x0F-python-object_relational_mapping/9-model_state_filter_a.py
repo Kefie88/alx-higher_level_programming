b91fc96c88b367b88cfe1e0fb2a2540b371b334a
Kefyalew <you@example.com>
2022-12-04 10:15:49 +0000

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    session = session(engine)
     data = session.query(State).order_by(State.id).filter(State.name.like("%a%")).all()
 
     for row in data:
