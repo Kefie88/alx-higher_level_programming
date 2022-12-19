51df01c515ccbc5b125ce24732474ec38208ca33
Kefyalew <you@example.com>
2022-12-19 05:46:47 +0000

    Late Commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Script that fetches from a url using urllib package"""
 
-import urllib.request as request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as respo:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as respo:
         cont = respo.read()
         print("Body response:")
         print("\t- type: {}".format(type(cont)))
