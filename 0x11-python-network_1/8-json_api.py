29bb94394f938209b921829ad5c3cdcab64fc255
Kefyalew <you@example.com>
2022-12-19 06:07:57 +0000

    Late Commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,10 +2,10 @@
 """Sends a POST request to http://0.0.0.0:5000 """
 
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    letter = "" if len(argv) == 1 else argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
     req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
