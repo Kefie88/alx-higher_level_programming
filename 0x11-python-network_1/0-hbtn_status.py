6ad57dc11e5eb2b7f5bb3b18539848a35e09f370
Kefyalew <you@example.com>
2022-12-19 05:37:47 +0000

    Late Commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request as request
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as respo:
         cont = respo.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(cont)))
         print("\t- content: {}".format(cont))
         print("\t- utf8 content: {}".format(cont.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ from sys import argv
 import urllib.request as req
 
 if __name__ == "__main__":
-    url = arg[1]
+    url = argv[1]
 
     with req.urlopen(url) as respo:
         print(dict(respo.headers).get("X-Request-Id"))
