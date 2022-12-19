8abb9823b1df25f06aef35cfc3e822b5a48ef62c
Kefyalew <you@example.com>
2022-12-19 05:43:17 +0000

    Late Commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 
 if __name__ == "__main__":
     respo = requests.get('https://intranet.hbtn.io/status')
-    print("Body response: \n\t- type: {}\n\t- content: {}"
-            .format(type(respo.text), respo.text))
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+        .format(type(respo.text), respo.text))
