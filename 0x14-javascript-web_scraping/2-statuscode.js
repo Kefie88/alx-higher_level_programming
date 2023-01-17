31804f2bc62f4e59751fe4342dbeee99c8a295c9
Kefyalew <you@example.com>
2023-01-17 12:08:58 +0000

    Javascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const args = process.argv;
 let request = require('request');
 request(args[2], function (error, response, body) {
   if (error) {
-    console.log('error:', error); // Print the error if one occurred
+    console.log('error:', error);
   } else console.log('code:', response && response.statusCode);
 });
