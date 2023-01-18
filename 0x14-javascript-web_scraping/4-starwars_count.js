5b201d685472b8e753a810ded4fb219e7bfb8921
Kefyalew <you@example.com>
2023-01-18 07:23:30 +0000

    Late Fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,7 +11,7 @@ request(reqURL, function (error, response, body) {
   } else {
     const jso = JSON.parse(body);
     const results = jso['results'];
-    const count = 0;
+    let count = 0;
     for (let i = 0; i < results.length; i++) {
       const chars = (results[i]['characters']);
       for (let j = 0; j < chars.length; j++) {
