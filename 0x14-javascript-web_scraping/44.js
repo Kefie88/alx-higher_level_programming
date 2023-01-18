ae926f19241f9226f86a678b417a91c671595452
Kefyalew <you@example.com>
2023-01-18 07:20:42 +0000

    Late Fix

--- 0x14-javascript-web_scraping/44.js
+++ 0x14-javascript-web_scraping/44.js
@@ -9,13 +9,13 @@ request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
-    const jso = JSON.parse(body);
-    const results = jso['results'];
+    let jso = JSON.parse(body);
+    let results = jso['results'];
     let count = 0;
     for (let i = 0; i < results.length; i++) {
-      const chars = (results[i]['characters']);
+      let chars = (results[i]['characters']);
       for (let j = 0; j < chars.length; j++) {
-        const check18 = chars[j].endsWith('18/');
+        let check18 = chars[j].endsWith('18/');
         if (check18) {
           count++;
         }
